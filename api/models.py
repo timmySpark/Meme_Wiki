eed6b9536dd9dc9dce8baabef172b08c5820b27a
timmy <timmyspark1@gmail.com>
2023-04-02 19:15:44 +0100

    [feat] :- created models for Jokes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Joke(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=100,default='none',null=False)
+    setup = models.TextField()
+    punchline = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    class Meta:
+        verbose_name = ("Joke")
+        verbose_name_plural = ("Jokes")
+
+    def __str__(self):
+        return self.type
+
+    def get_absolute_url(self):
+        return reverse("Joke_detail", kwargs={"pk": self.pk})
+
+
+
+# class Meme(models.Model):
+    
+    
+
+#     class Meta:
+#         verbose_name = ("Meme")
+#         verbose_name_plural = ("Memes")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("Meme_detail", kwargs={"pk": self.pk})
